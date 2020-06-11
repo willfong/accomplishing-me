99c8657a4e30531df194d5330fcffe47743dc22a
Will Fong <willfong@pm.me>
2020-06-11 10:02:45 +0800

    Refactor to use millisecond as keys

--- src/components/TodaysActivity.vue
+++ src/components/TodaysActivity.vue
@@ -10,7 +10,7 @@
         </div>
     </div>
     <ul>
-        <li v-for="ts in Object.keys(activities)" :key="ts">{{activities[ts]}} <small class="is-pulled-right">{{moment.unix(ts).fromNow()}}</small></li>
+        <li v-for="ts in Object.keys(activities)" :key="ts">{{activities[ts]}} <small class="is-pulled-right">{{moment(parseInt(ts, 10)).fromNow()}}</small></li>
     </ul>
     <!--
     <hr />
@@ -37,7 +37,7 @@ export default {
   },
   methods: {
     addNewActivity() {
-        const currentTs = Math.floor(Date.now() / 1000)
+        const currentTs = Date.now();
         let activity = {};
         activity[currentTs] = this.newActivity;
         this.$store.dispatch('activitiesNew', activity);
